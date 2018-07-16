f41a784631703acbf7af3183a3724486e047f855
Wesley Walter Anton Oliver <wesley.olis@gmail.com>
2018-07-16 14:31:05 +0200

    Added some tests for BlueBird Promisify

--- src/util/bluebird-promisify.ts
+++ src/util/bluebird-promisify.ts
@@ -1,4 +1,5 @@
 import * as Bluebird from 'bluebird';
+import { callbackify } from 'util';
 
 /* tslint:disable:max-line-length */
 
@@ -17,40 +18,57 @@ export type ParamH<T> = T extends (A: any, B: any, C: any, D: any, E: any, F: an
 export type ParamI<T> = T extends (A: any, B: any, C: any, D: any, E: any, F: any, G: any, H: any, I: infer P , ... args: any []) => void ? P : never;
 export type Paramlast = (A: any, B: any, C: any, D: any, E: any, F: any, G: any, H: any, I: any , ... args: any []) => void;
 
+
+const promisify = Promisify(function(paramA : boolean, callback : (err : number, result : CallBackType) => void) {})
+
+
+export function Promisify<M>(propA: M)
+ : ParamI<M> extends CallBackType ? (A: ParamA<M>, B: ParamB<M>, C: ParamC<M>, D: ParamD<M>, E: ParamE<M>, F: ParamF<M>, G: ParamG<M>, H: ParamH<M>) => Bluebird<RCallBack<ParamI<M>>>
+: ParamH<M> extends CallBackType ? (A: ParamA<M>, B: ParamB<M>, C: ParamC<M>, D: ParamD<M>, E: ParamE<M>, F: ParamF<M>, G: ParamG<M>) => Bluebird<RCallBack<ParamH<M>>>
+: ParamG<M> extends CallBackType ? (A: ParamA<M>, B: ParamB<M>, C: ParamC<M>, D: ParamD<M>, E: ParamE<M>, F: ParamF<M>) => Bluebird<RCallBack<ParamG<M>>>
+: ParamF<M> extends CallBackType ? (A: ParamA<M>, B: ParamB<M>, C: ParamC<M>, D: ParamD<M>, E: ParamE<M>) => Bluebird<RCallBack<ParamF<M>>>
+: ParamE<M> extends CallBackType ? (A: ParamA<M>, B: ParamB<M>, C: ParamC<M>, D: ParamD<M>) => Bluebird<RCallBack<ParamE<M>>>
+: ParamD<M> extends CallBackType ? (A: ParamA<M>, B: ParamB<M>, C: ParamC<M>) => Bluebird<RCallBack<ParamD<M>>>
+: ParamC<M> extends CallBackType ? (A: ParamA<M>, B: ParamB<M>) => Bluebird<RCallBack<ParamC<M>>>
+: ParamB<M> extends CallBackType ? (A: ParamA<M>) => Bluebird<RCallBack<ParamB<M>>>
+: ParamA<M> extends CallBackType ? Bluebird<void>
+: M extends Paramlast ? 'Expanded the parameter templates' : '** Invalid Method (key) not found in object**';
+
+
 export function Promisify<O extends {}, M extends keyof O>(propA: O , propB: M)
-: ParamI<O[M]> extends CallBackType ? (A: ParamA<O[M]>, B: ParamB<O[M]>, C: ParamC<O[M]>, D: ParamD<O[M]>, E: ParamE<O[M]>, F: ParamF<O[M]>, G: ParamE<O[M]>, H: ParamE<O[M]>) => Bluebird<RCallBack<ParamI<O[M]>>>
-: ParamH<O[M]> extends CallBackType ? (A: ParamA<O[M]>, B: ParamB<O[M]>, C: ParamC<O[M]>, D: ParamD<O[M]>, E: ParamE<O[M]>, F: ParamF<O[M]>, G: ParamE<O[M]>) => Bluebird<RCallBack<ParamH<O[M]>>>
+: ParamI<O[M]> extends CallBackType ? (A: ParamA<O[M]>, B: ParamB<O[M]>, C: ParamC<O[M]>, D: ParamD<O[M]>, E: ParamE<O[M]>, F: ParamF<O[M]>, G: ParamG<O[M]>, H: ParamH<O[M]>) => Bluebird<RCallBack<ParamI<O[M]>>>
+: ParamH<O[M]> extends CallBackType ? (A: ParamA<O[M]>, B: ParamB<O[M]>, C: ParamC<O[M]>, D: ParamD<O[M]>, E: ParamE<O[M]>, F: ParamF<O[M]>, G: ParamG<O[M]>) => Bluebird<RCallBack<ParamH<O[M]>>>
 : ParamG<O[M]> extends CallBackType ? (A: ParamA<O[M]>, B: ParamB<O[M]>, C: ParamC<O[M]>, D: ParamD<O[M]>, E: ParamE<O[M]>, F: ParamF<O[M]>) => Bluebird<RCallBack<ParamG<O[M]>>>
 : ParamF<O[M]> extends CallBackType ? (A: ParamA<O[M]>, B: ParamB<O[M]>, C: ParamC<O[M]>, D: ParamD<O[M]>, E: ParamE<O[M]>) => Bluebird<RCallBack<ParamF<O[M]>>>
 : ParamE<O[M]> extends CallBackType ? (A: ParamA<O[M]>, B: ParamB<O[M]>, C: ParamC<O[M]>, D: ParamD<O[M]>) => Bluebird<RCallBack<ParamE<O[M]>>>
 : ParamD<O[M]> extends CallBackType ? (A: ParamA<O[M]>, B: ParamB<O[M]>, C: ParamC<O[M]>) => Bluebird<RCallBack<ParamD<O[M]>>>
 : ParamC<O[M]> extends CallBackType ? (A: ParamA<O[M]>, B: ParamB<O[M]>) => Bluebird<RCallBack<ParamC<O[M]>>>
 : ParamB<O[M]> extends CallBackType ? (A: ParamA<O[M]>) => Bluebird<RCallBack<ParamB<O[M]>>>
-: ParamA<O[M]> extends CallBackType ? () => Bluebird<RCallBack<ParamA<O[M]>>>
+: ParamA<O[M]> extends CallBackType ? Bluebird<RCallBack<ParamA<O[M]>>>
 : O[M] extends Paramlast ? 'Expanded the parameter templates' : '** Invalid Method (key) not found in object**';
 
 export function Promisify<R, M extends (...args: any []) => void>(propA: M)
-: ParamI<M> extends CallBackType ? (A: ParamA<M>, B: ParamB<M>, C: ParamC<M>, D: ParamD<M>, E: ParamE<M>, F: ParamF<M>, G: ParamE<M>, H: ParamE<M>) => Bluebird<R>
-: ParamH<M> extends CallBackType ? (A: ParamA<M>, B: ParamB<M>, C: ParamC<M>, D: ParamD<M>, E: ParamE<M>, F: ParamF<M>, G: ParamE<M>) => Bluebird<R>
+: ParamI<M> extends CallBackType ? (A: ParamA<M>, B: ParamB<M>, C: ParamC<M>, D: ParamD<M>, E: ParamE<M>, F: ParamF<M>, G: ParamG<M>, H: ParamE<M>) => Bluebird<R>
+: ParamH<M> extends CallBackType ? (A: ParamA<M>, B: ParamB<M>, C: ParamC<M>, D: ParamD<M>, E: ParamE<M>, F: ParamF<M>, G: ParamG<M>) => Bluebird<R>
 : ParamG<M> extends CallBackType ? (A: ParamA<M>, B: ParamB<M>, C: ParamC<M>, D: ParamD<M>, E: ParamE<M>, F: ParamF<M>) => Bluebird<R>
 : ParamF<M> extends CallBackType ? (A: ParamA<M>, B: ParamB<M>, C: ParamC<M>, D: ParamD<M>, E: ParamE<M>) => Bluebird<R>
 : ParamE<M> extends CallBackType ? (A: ParamA<M>, B: ParamB<M>, C: ParamC<M>, D: ParamD<M>) => Bluebird<R>
 : ParamD<M> extends CallBackType ? (A: ParamA<M>, B: ParamB<M>, C: ParamC<M>) => Bluebird<R>
 : ParamC<M> extends CallBackType ? (A: ParamA<M>, B: ParamB<M>) => Bluebird<R>
 : ParamB<M> extends CallBackType ? (A: ParamA<M>) => Bluebird<R>
-: ParamA<M> extends CallBackType ? () => Bluebird<R>
+: ParamA<M> extends CallBackType ? Bluebird<R>
 : M extends Paramlast ? 'Expanded the parameter templates' : '** Invalid Method (key) not found in object**';
 
 export function Promisify<R, O extends {}, M extends keyof O>(propA: O , propB: M)
-: ParamI<O[M]> extends CallBackType ? (A: ParamA<O[M]>, B: ParamB<O[M]>, C: ParamC<O[M]>, D: ParamD<O[M]>, E: ParamE<O[M]>, F: ParamF<O[M]>, G: ParamE<O[M]>, H: ParamE<O[M]>) => Bluebird<R>
-: ParamH<O[M]> extends CallBackType ? (A: ParamA<O[M]>, B: ParamB<O[M]>, C: ParamC<O[M]>, D: ParamD<O[M]>, E: ParamE<O[M]>, F: ParamF<O[M]>, G: ParamE<O[M]>) => Bluebird<R>
+: ParamI<O[M]> extends CallBackType ? (A: ParamA<O[M]>, B: ParamB<O[M]>, C: ParamC<O[M]>, D: ParamD<O[M]>, E: ParamE<O[M]>, F: ParamF<O[M]>, G: ParamG<O[M]>, H: ParamH<O[M]>) => Bluebird<R>
+: ParamH<O[M]> extends CallBackType ? (A: ParamA<O[M]>, B: ParamB<O[M]>, C: ParamC<O[M]>, D: ParamD<O[M]>, E: ParamE<O[M]>, F: ParamF<O[M]>, G: ParamG<O[M]>) => Bluebird<R>
 : ParamG<O[M]> extends CallBackType ? (A: ParamA<O[M]>, B: ParamB<O[M]>, C: ParamC<O[M]>, D: ParamD<O[M]>, E: ParamE<O[M]>, F: ParamF<O[M]>) => Bluebird<R>
 : ParamF<O[M]> extends CallBackType ? (A: ParamA<O[M]>, B: ParamB<O[M]>, C: ParamC<O[M]>, D: ParamD<O[M]>, E: ParamE<O[M]>) => Bluebird<R>
 : ParamE<O[M]> extends CallBackType ? (A: ParamA<O[M]>, B: ParamB<O[M]>, C: ParamC<O[M]>, D: ParamD<O[M]>) => Bluebird<R>
 : ParamD<O[M]> extends CallBackType ? (A: ParamA<O[M]>, B: ParamB<O[M]>, C: ParamC<O[M]>) => Bluebird<R>
 : ParamC<O[M]> extends CallBackType ? (A: ParamA<O[M]>, B: ParamB<O[M]>) => Bluebird<R>
 : ParamB<O[M]> extends CallBackType ? (A: ParamA<O[M]>) => Bluebird<R>
-: ParamA<O[M]> extends CallBackType ? () => Bluebird<R>
+: ParamA<O[M]> extends CallBackType ? Bluebird<R>
 : O[M] extends Paramlast ? 'Expanded the parameter templates' : '** Invalid Method (key) not found in object**';
 
 /* tslint:enable:max-line-length */
@@ -58,13 +76,73 @@ export function Promisify<R, O extends {}, M extends keyof O>(propA: O , propB: 
 // tslint:disable-next-line
 export function Promisify(propA: any, propB: any = undefined): any {
     if (propB === undefined) {
-        return Bluebird.promisify(propA);
+
+        if (propA.length == 1){
+            return Bluebird.promisify(propA)(); // require to output the results if needed.
+        } else {
+            return Bluebird.promisify(propA);
+        }
+
     } else if (propA[propB]) {
-        return Bluebird.promisify(propA[propB], {context: propA});
+
+        if (propA[propB].length == 1){
+            return Bluebird.promisify(propA[propB], {context: propA})();
+        } else {
+            return Bluebird.promisify(propA[propB], {context: propA});
+        }
+        
     } else {
         throw Error(`Error key [${propB}] not found on object`);
     }
 }
+/*
+export function PromisifyWithExecResult(propA: any, propB: any = undefined): (A :any, execR : {execR: any} | undefined) => Bluebird<R> {
+
+    return new Bluebird(function(resolve: any, reject : any){
+
+            if (propB == undefined)
+            {
+               let func : (args: any []) => any;
+                 
+                    return ( jj: any [], execR : {execR: any} | undefined = undefined) : Bluebird<R> => { 
+                        new Bluebird(function(resolve, reject) {
+                            
+                            // the problem is that we now have an additional parameter, which I now need to detect
+                            // ontop of a callback., basically if the last argument is unein
+
+                            // the last arguments could be a Function, or callback undefined, then their could be confusion,
+                            // I will have to check the following.
+
+                            const lastArg = arguments.length - 1;
+                            const lastArgUndefined = lastArg >= 0 && arguments[lastArg] === undefined;
+
+                            
+
+                            let args : any [] = [];
+                            let i = arguments.length - 1;
+                            while(i--)
+                                args.push(arguments[i]);
+                            
+                            args.push(resolve);
+                            
+                            try {
+
+                                let result = func.call(args, resolve);
+                                if(execR !== undefined)
+                                    execR.execR = result;
+
+                            } catch(e) {
+                                reject(e);
+                            }
+                        }
+                    }
+            
+            }
+        }).;
+
+        // I will have to unravel BlueBird.promisify, so that I do that step manauall my self..
+    });
+*/
 
 // const testsCallBack = {
 // 	test : function (callback : (err : any, result : Inter) => void),
---
+++ test/util/bluebird-promisify.spec.ts
@@ -0,0 +1,241 @@
+import * as chai from 'chai'
+import {Promisify} from '../../src/util/bluebird-promisify';
+import { onPossiblyUnhandledRejection } from 'bluebird';
+const sut = require('redblade-testing');
+
+type CallBack = (err: any, result: any) => void;
+
+type ControlState = {
+    succes : any | undefined;
+    error : any | undefined;
+};
+
+function newControlState() : ControlState {
+        
+    return {
+        succes : undefined,
+        error : undefined
+    }
+}
+
+function wait(control : ControlState, callback : CallBack)
+{
+    if (control.succes !== undefined)
+        return callback(undefined, control.succes);
+    else if (control.error !== undefined)
+        return callback(control.error, undefined);
+    else
+        setTimeout(() => wait(control, callback), 10);
+}
+
+
+class TestObject
+{
+    public control : ControlState;
+
+    constructor(public prefix : string)
+    {
+        this.control = newControlState();
+    }
+
+    fun(callback : (err: any, result: boolean) => void) : void
+    {
+        this.waitObject(this.control, callback);
+    }
+
+    funParamA(paramA : 'A', callback : (err: any, result: string) => void) : void
+    {
+        this.waitObject(this.control, callback);
+    }
+    
+    funParamH(paramA : 'A', paramB : 'B', paramC : 'C', paramD : 'D', paramE : 'E', paramF : 'F', paramG : 'G', paramH : 'H', callback : (err: any, result: number) => void) : void
+    {
+        this.waitObject(this.control, callback);
+    }
+
+
+    waitObject(control : ControlState, callback : CallBack)
+    {
+        if (control.succes !== undefined)
+            return callback(undefined, this.prefix + ":" + control.succes);
+        else if (control.error !== undefined)
+            return callback(this.prefix + ":" + control.error, undefined);
+        else
+            setTimeout(() => this.waitObject(control, callback), 10);
+    }
+}
+
+
+describe("Generic Promisification", function() {
+
+    describe("funtion only", function()
+    {
+        function genCallBackFun<C extends CallBack>(control : ControlState)
+        {
+            return function(callback : C)
+            {
+                wait(control, callback);
+            }
+        }
+
+        function genCallBackFunWithParamA<C extends CallBack>(control : ControlState)
+        {
+            return function(paramA : 'A', callback : C)
+            {
+                wait(control, callback);
+            }
+        }
+
+        function genCallBackFunWithParamH<C extends CallBack>(control : ControlState)
+        {
+            return function(paramA : 'A', paramB : 'B', paramC : 'C', paramD : 'D', paramE : 'E', paramF : 'F', paramG : 'G', paramH : 'H', callback : C)
+            {
+                wait(control, callback);
+            }
+        }
+
+        describe("With Results", function() {
+            it("No Paramss", async function() {
+
+                let control = newControlState();
+    
+                const funToBePromisified = genCallBackFun<(err: any, result: string) => void>(control);
+    
+                const promisifyFun = Promisify(funToBePromisified);
+    
+                control.succes = 'sucess_1';
+    
+                chai.expect(await promisifyFun).to.eql('sucess_1');
+            });
+    
+    
+            it("WithPrams", async function() {
+    
+                let control = newControlState();
+    
+                const funToBePromisified = genCallBackFunWithParamA<(err: any, result: string) => void>(control);
+    
+                const promisifyFun = Promisify(funToBePromisified);
+    
+                const resultPromsie = promisifyFun('A')
+    
+                control.succes = 'sucess_A';
+    
+                chai.expect(await resultPromsie).to.eql('sucess_A');
+            });
+    
+    
+    
+            it("WithPramsH", async function() {
+    
+                let control = newControlState();
+    
+                const funToBePromisified = genCallBackFunWithParamH<(err: any, result: string) => void>(control);
+    
+                const promisifyFun = Promisify(funToBePromisified);
+    
+                const resultPromsie = promisifyFun('A','B','C','D','E','F','G','H')
+    
+                control.succes = 'sucess_H';
+    
+                chai.expect(await resultPromsie).to.eql('sucess_H');
+            });
+        });
+
+
+        describe("With Error", function() {
+
+            it("No Params", async function() {
+
+                let control = newControlState();
+    
+                const funToBePromisified = genCallBackFun<(err: any, result: string) => void>(control);
+    
+                const promisifyFun = Promisify(funToBePromisified);
+    
+                control.error = 'error_1';
+
+                chai.expect(await sut.returnCaughtErrorAsync(promisifyFun)).property('message').to.eq(control.error);
+            });
+    
+    
+            it("WithPrams", async function() {
+    
+                let control = newControlState();
+    
+                const funToBePromisified = genCallBackFunWithParamA<(err: any, result: string) => void>(control);
+    
+                const promisifyFun = Promisify(funToBePromisified);
+    
+                const resultPromsie = promisifyFun('A')
+    
+                control.error = 'error_A';
+    
+                chai.expect(await sut.returnCaughtErrorAsync(resultPromsie)).property('message').to.eq(control.error);
+            });
+    
+    
+    
+            it("WithPramsH", async function() {
+    
+                let control = newControlState();
+    
+                const funToBePromisified = genCallBackFunWithParamH<(err: any, result: string) => void>(control);
+    
+                const promisifyFun = Promisify(funToBePromisified);
+    
+                const resultPromsie = promisifyFun('A','B','C','D','E','F','G','H')
+    
+                control.error = 'error_H';
+    
+                chai.expect(await sut.returnCaughtErrorAsync(resultPromsie)).property('message').to.eq(control.error);
+            });
+        });  
+    });
+
+    describe("Object with function", function()
+    {
+        it("No Paramss", async function() {
+
+            const objectFunToBePromisified = new TestObject("InitPrefix");
+            
+            const promisify = Promisify(objectFunToBePromisified, 'fun');
+
+            objectFunToBePromisified.prefix = "InstanceNoParams";
+            objectFunToBePromisified.control.succes = 'sucess';
+
+            chai.expect(await promisify).to.eql(objectFunToBePromisified.prefix + ":" + objectFunToBePromisified.control.succes);
+        });
+
+
+        it("WithPrams", async function() {
+
+            const objectFunToBePromisified = new TestObject("InitPrefix");
+            
+            const promisifyFun = Promisify(objectFunToBePromisified, 'funParamA');
+
+            const promise = promisifyFun('A');
+
+            objectFunToBePromisified.prefix = "InstanceParamsA";
+            objectFunToBePromisified.control.succes = 'sucessA';
+
+            chai.expect(await promise).to.eql(objectFunToBePromisified.prefix + ":" + objectFunToBePromisified.control.succes);
+        });
+
+
+
+        it("WithPramsH", async function() {
+
+            const objectFunToBePromisified = new TestObject("InitPrefix");
+            
+            const promisifyFun = Promisify(objectFunToBePromisified, 'funParamH');
+
+            const promise = promisifyFun('A','B','C','D','E','F','G','H');
+
+            objectFunToBePromisified.prefix = "InstanceParamsH";
+            objectFunToBePromisified.control.succes = 'sucessH';
+
+            chai.expect(await promise).to.eql(objectFunToBePromisified.prefix + ":" + objectFunToBePromisified.control.succes);
+        });
+    });
+});
