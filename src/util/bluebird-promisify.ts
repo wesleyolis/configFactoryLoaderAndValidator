a30df23c70d354042ad35d78ee25d6767f3161f4
Wesley Walter Anton Oliver <wesley.olis@gmail.com>
2018-07-16 17:01:21 +0200

    Added PromisifyReturn

--- src/util/bluebird-promisify.ts
+++ src/util/bluebird-promisify.ts
@@ -69,6 +69,7 @@ export function Promisify<R, O extends {}, M extends keyof O>(propA: O , propB: 
 
 /* tslint:enable:max-line-length */
 
+
 // tslint:disable-next-line
 export function Promisify(propA: any, propB: any = undefined): any {
     if (propB === undefined) {
@@ -91,83 +92,55 @@ export function Promisify(propA: any, propB: any = undefined): any {
         throw Error(`Error key [${propB}] not found on object`);
     }
 }
+
 /*
-export function PromisifyWithExecResult(propA: any, propB: any = undefined): (A :any, execR : {execR: any} | undefined) => Bluebird<R> {
+export function PromisifyReturn(propA : any, propB : any = undefined) : (...args : any[]) => Bluebird<any>
+{
+    function generatePromise (func : Function, object : any) : (...args : any[]) => Bluebird<any> 
+    {
+        return (execute : {[index:string] : any} | undefined) : Bluebird<any> => { 
+       
+            return new Bluebird(function(resolve, reject) {
+    
+                let self : any | undefined = object;
+                
+                const lastArg = arguments.length - 1;
+                const lastArgUndefined = lastArg >= 0 && arguments[lastArg] === undefined;
+    
+                let args : any [] = [];
+                let i = arguments.length - 1;
+                while(i--)
+                    args.push(arguments[i]);
+                
+                //args.push(resolve);
+                
+                try {
+                    let result = undefined;
 
-    return new Bluebird(function(resolve: any, reject : any){
+                    if (self)
+                        result = func.call(self, ...args, resolve);
+                    else
+                        result = func(...args, resolve);
 
-            if (propB == undefined)
-            {
-               let func : (args: any []) => any;
-                 
-                    return ( jj: any [], execR : {execR: any} | undefined = undefined) : Bluebird<R> => { 
-                        new Bluebird(function(resolve, reject) {
-                            
-                            // the problem is that we now have an additional parameter, which I now need to detect
-                            // ontop of a callback., basically if the last argument is unein
+                    if(arguments[lastArg])
+                        arguments[lastArg]['executeResult'] = result;
+    
+                } catch(e) {
+                    reject(e);
+                }
+            });
+        };
+    }
 
-                            // the last arguments could be a Function, or callback undefined, then their could be confusion,
-                            // I will have to check the following.
-
-                            const lastArg = arguments.length - 1;
-                            const lastArgUndefined = lastArg >= 0 && arguments[lastArg] === undefined;
-
-                            
-
-                            let args : any [] = [];
-                            let i = arguments.length - 1;
-                            while(i--)
-                                args.push(arguments[i]);
-                            
-                            args.push(resolve);
-                            
-                            try {
-
-                                let result = func.call(args, resolve);
-                                if(execR !== undefined)
-                                    execR.execR = result;
-
-                            } catch(e) {
-                                reject(e);
-                            }
-                        }
-                    }
-            
-            }
-        }).;
-
-        // I will have to unravel BlueBird.promisify, so that I do that step manauall my self..
-    });
+    if (propB === undefined)
+    {
+        return generatePromise(propA, undefined);
+    }
+    else if (propA[propB])
+    {
+        return generatePromise(propB, propA[propB]);
+    }
+    else
+        throw Error(`Error key [${propB}] not found on object`);
+}
 */
-
-// const testsCallBack = {
-// 	test : function (callback : (err : any, result : Inter) => void),
-// 	paramA : function (ParamA : 1, callback : (err : any, result : 'AA') => void),
-// 	paramB : function (ParamA : 1, ParamB : 2, callback : (err : any, result : 'BB') => void),
-// 	paramC : function (ParamA : 1, ParamB : 2, ParamC : 3,  callback : (err : any, result : 'CC') => void),
-// 	paramD : function (ParamA : 1, ParamB : 2, ParamC : 3, ParamD : 4, callback : (err : any, result : 'DD') => void)
-// }
-
-// interface Inter {
-//     propA : string;
-//     propB : number;
-// }
-
-// interface Overide {
-//     overideA : string;
-//     overideB : string;
-// }
-
-// // Still require the parameters.
-// const test = Promisify(testsCallBack,'test');
-// test();
-
-// // Still require the parameters.
-// const paramD = Promisify<Overide, typeof testsCallBack, 'paramD'>(testsCallBack,'paramD');
-// paramD();
-
-// // Still require the parameters.
-// const paramC = Promisify<Overide, typeof testsCallBack.paramC>(testsCallBack.paramC);
-// paramC().then((r) => {
-//   r.overideB
-// });
--- test/util/bluebird-promisify.spec.ts
+++ test/util/bluebird-promisify.spec.ts
@@ -243,3 +243,192 @@ describe("Generic Promisification", function() {
         });
     });
 });
+
+/*
+
+describe("Promisification with return", function() {
+
+    describe("funtion only", function()
+    {
+        function genCallBackFun<C extends CallBack, R>(control : ControlState, initResult : R)
+        {
+            return function(callback : C) : R
+            {
+                wait(control, '', callback);
+
+                return initResult;
+            }
+        }
+
+        function genCallBackFunWithParamA<C extends CallBack, R>(control : ControlState, initResult : R)
+        {
+            return function(paramA : 'A', callback : C) : R
+            {
+                wait(control, 'A', callback);
+
+                return initResult;
+            }
+        }
+
+        function genCallBackFunWithParamH<C extends CallBack, R>(control : ControlState, initResult : R)
+        {
+            return function(paramA : 'A', paramB : 'B', paramC : 'C', paramD : 'D', paramE : 'E', paramF : 'F', paramG : 'G', paramH : 'H', callback : C) : R
+            {
+                wait(control, 'H', callback);
+
+                return initResult;
+            }
+        }
+
+        describe("With Results", function() {
+
+            it.only("No Params", async function() {
+
+                let control = newControlState();
+    
+                const funToBePromisified = genCallBackFun<(err: any, result: string) => string, 'ReturnNoParms'>(control, 'ReturnNoParms');
+    
+                const promisifyFun = PromisifyReturn(funToBePromisified);
+
+                const initReturn = {initObject: 'initObject'};
+
+                const promisify = promisifyFun(initReturn);
+    
+                control.succes = 'sucess_1';
+    
+                chai.expect(await promisify).to.eql(':sucess_1');
+            });
+    
+    
+            // it("WithPrams", async function() {
+    
+            //     let control = newControlState();
+    
+            //     const funToBePromisified = genCallBackFunWithParamA<(err: any, result: string) => void, 'ReturnParamA'>(control,'ReturnParamA');
+    
+            //     const promisifyFun = PromisifyReturn(funToBePromisified);
+    
+            //     const resultPromsie = promisifyFun('A',)
+    
+            //     control.succes = 'sucess_A';
+    
+            //     chai.expect(await resultPromsie).to.eql('A:sucess_A');
+            // });
+    
+    
+    
+            // it("WithPramsH", async function() {
+    
+            //     let control = newControlState();
+    
+            //     const funToBePromisified = genCallBackFunWithParamH<(err: any, result: string) => void>(control);
+    
+            //     const promisifyFun = Promisify(funToBePromisified);
+    
+            //     const resultPromsie = promisifyFun('A','B','C','D','E','F','G','H')
+    
+            //     control.succes = 'sucess_H';
+    
+            //     chai.expect(await resultPromsie).to.eql('H:sucess_H');
+            // });
+        });
+    });
+
+
+    //     describe("With Error", function() {
+
+    //         it("No Params", async function() {
+
+    //             let control = newControlState();
+    
+    //             const funToBePromisified = genCallBackFun<(err: any, result: string) => void>(control);
+    
+    //             const promisifyFun = Promisify(funToBePromisified);
+    
+    //             control.error = 'error_1';
+
+    //             chai.expect(await sut.returnCaughtErrorAsync(promisifyFun)).property('message').to.eq(control.error);
+    //         });
+    
+    
+    //         it("WithPrams", async function() {
+    
+    //             let control = newControlState();
+    
+    //             const funToBePromisified = genCallBackFunWithParamA<(err: any, result: string) => void>(control);
+    
+    //             const promisifyFun = Promisify(funToBePromisified);
+    
+    //             const resultPromsie = promisifyFun('A')
+    
+    //             control.error = 'error_A';
+    
+    //             chai.expect(await sut.returnCaughtErrorAsync(resultPromsie)).property('message').to.eq(control.error);
+    //         });
+    
+    
+    
+    //         it("WithPramsH", async function() {
+    
+    //             let control = newControlState();
+    
+    //             const funToBePromisified = genCallBackFunWithParamH<(err: any, result: string) => void>(control);
+    
+    //             const promisifyFun = Promisify(funToBePromisified);
+    
+    //             const resultPromsie = promisifyFun('A','B','C','D','E','F','G','H')
+    
+    //             control.error = 'error_H';
+    
+    //             chai.expect(await sut.returnCaughtErrorAsync(resultPromsie)).property('message').to.eq(control.error);
+    //         });
+    //     });  
+    // });
+
+    // describe("Object with function", function()
+    // {
+    //     it("No Paramss", async function() {
+
+    //         const objectFunToBePromisified = new TestObject("InitPrefix");
+            
+    //         const promisify = Promisify(objectFunToBePromisified, 'fun');
+
+    //         objectFunToBePromisified.prefix = "InstanceNoParams";
+    //         objectFunToBePromisified.control.succes = 'sucess';
+
+    //         chai.expect(await promisify).to.eql(objectFunToBePromisified.prefix + "::" + objectFunToBePromisified.control.succes);
+    //     });
+
+
+    //     it("WithPrams", async function() {
+
+    //         const objectFunToBePromisified = new TestObject("InitPrefix");
+            
+    //         const promisifyFun = Promisify(objectFunToBePromisified, 'funParamA');
+
+    //         const promise = promisifyFun('A');
+
+    //         objectFunToBePromisified.prefix = "InstanceParamsA";
+    //         objectFunToBePromisified.control.succes = 'sucessA';
+
+    //         chai.expect(await promise).to.eql(objectFunToBePromisified.prefix + ":A:" + objectFunToBePromisified.control.succes);
+    //     });
+
+
+
+    //     it("WithPramsH", async function() {
+
+    //         const objectFunToBePromisified = new TestObject("InitPrefix");
+            
+    //         const promisifyFun = Promisify(objectFunToBePromisified, 'funParamH');
+
+    //         const promise = promisifyFun('A','B','C','D','E','F','G','H');
+
+    //         objectFunToBePromisified.prefix = "InstanceParamsH";
+    //         objectFunToBePromisified.control.succes = 'sucessH';
+
+    //         chai.expect(await promise).to.eql(objectFunToBePromisified.prefix + ":H:" + objectFunToBePromisified.control.succes);
+    //     });
+    // });
+});
+*/
