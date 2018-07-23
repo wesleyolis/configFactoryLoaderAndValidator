c069e64e08093327401827e9007d3f07e9841c76
Wesley Walter Anton Oliver <wesley.olis@gmail.com>
2018-07-23 10:20:06 +0200

    Added support to reload the global configuration, wiht lazy loading, because of all the missing configuration

--- lib/config/index.d.ts
+++ lib/config/index.d.ts
@@ -2,3 +2,4 @@ import { ConfigSchema, configSchema } from './global';
 import { LoadedConfig } from '../index';
 export { ConfigSchema as ConfigSchema };
 export declare function configAsync(): Promise<LoadedConfig<typeof configSchema>>;
+export declare function loadConfig(lazyLoad?: boolean, configOptional?: boolean, configSettings?: any): Promise<void>;
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,10 @@
 import {ConfigSchema, configSchema} from './global'
-import {LoadConfig, LoadedConfig } from '../index'
+import {LoadConfig as genericLoadConfig, LoadedConfig } from '../index'
 import { some } from 'bluebird';
 export {ConfigSchema as ConfigSchema};
+import {} from 'redblade-types'
+
+const rbLog = global.rbLog;
 
 let configInstancesLoaded : LoadedConfig<typeof configSchema> | undefined = undefined; 
 
@@ -16,8 +19,16 @@ export async function configAsync () : Promise<LoadedConfig<typeof configSchema>
         // this should be replace with direct memory load of configuration, that is passed in via the enviroment.
         // their should be no file loading at all.
 
-        configInstancesLoaded = await LoadConfig(config, configSchema, true);
+        configInstancesLoaded = await genericLoadConfig(config, configSchema, true);
     }
     
     return Promise.resolve(configInstancesLoaded);
+}
+
+export async function loadConfig(lazyLoad : boolean = false, configOptional : boolean = false, configSettings : any = undefined) : Promise<void>
+{
+    if (configSettings == undefined)
+        configSettings = require('config');
+    
+    configInstancesLoaded = await genericLoadConfig(configSettings, configSchema, lazyLoad, configOptional);
 }
