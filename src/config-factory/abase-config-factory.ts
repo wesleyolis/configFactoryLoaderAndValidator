4b4895ce87cc807952a644dbb29f68f117b67d0e
Wesley Walter Anton Oliver <wesley.olis@gmail.com>
2018-07-23 12:36:15 +0200

    possible bux, fix, can't seem to a global property

--- src/config-factory/abase-config-factory.ts
+++ src/config-factory/abase-config-factory.ts
@@ -32,7 +32,9 @@ export abstract class ABaseConfigFactory extends Config implements IConfigFactor
     {
         try
         {
-            const configSchema = this.configSchema.keys(baseConfigSchema);
+            const baseTobeEnhanceConfigSchema = baseConfigSchema;
+
+            const configSchema = this.configSchema.keys(baseTobeEnhanceConfigSchema);
             this.configSettings = JSON.parse(JSON.stringify(await JoiX.validate(config, configSchema, {abortEarly: false})));
         }
         catch(e)
